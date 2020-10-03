dfb51e468e64e9adfd40ca8d923df46044792a9a
Konstantin Voykov <koykov0@gmail.com>
2020-10-04 01:07:10 +0300

    Make decrypt code common.

--- doubleclick/adid.go
+++ doubleclick/adid.go
@@ -1,28 +1,15 @@
 package doubleclick
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
 	"errors"
 	"fmt"
 )
 
 const (
-	adidMsgLen              = 36
-	adidInitVectorOffset    = 0
-	adidInitVectorLen       = 16
-	adidCipherOffset        = 16
-	adidCipherLen           = 16
-	adidIntegritySignOffset = 32
-	adidIntegritySignLen    = 4
-
-	adidBufPadOffset     = 0
-	adidBufPadLen        = 20
-	adidBufPayloadOffset = 20
-	adidBufPayloadLen    = 16
-	adidBufSignOffset    = 36
-	adidBufSignLen       = 20 // integrity signature needs only 4 bytes buffer, but hmac makes 20-bytes array by default
-	adidBufLen           = adidBufPadLen + adidBufPayloadLen + adidBufSignLen
+	adidMsgLen    = 36
+	adidCipherLen = 16
+	// integrity signature needs only 4 bytes buffer, but hmac makes 20-bytes array by default
+	adidBufLen = 56 // adidBufPadLen + adidBufPayloadLen + adidBufSignLen
 )
 
 type AdID struct {
@@ -51,58 +38,24 @@ func (a *AdID) Decrypt(dst, encryptedAdID []byte, uuid bool) ([]byte, error) {
 		return dst, ErrBadAdIDLen
 	}
 
-	initVector := encryptedAdID[adidInitVectorOffset:adidInitVectorLen]
-	cipherText := encryptedAdID[adidCipherOffset : adidCipherOffset+adidCipherLen]
-	integritySign := encryptedAdID[adidIntegritySignOffset : adidIntegritySignOffset+adidIntegritySignLen]
-
-	// Prepare buffer.
-	if len(a.buf) < adidBufLen {
-		a.buf = append(a.buf, make([]byte, adidBufLen-len(a.buf))...)
-	}
-
-	// Init encryption hmac.
-	if a.hmacE == nil {
-		a.hmacE = hmac.New(sha1.New, a.encryptionKey)
-	}
-	// Compute pad.
-	pad := a.buf[adidBufPadOffset:adidBufPadLen]
-	a.hmacE.Reset()
-	a.hmacE.Write(initVector)
-	pad = a.hmacE.Sum(pad[:0])
-
-	// Apply xor to reverse encryption.
-	payload := a.buf[adidBufPayloadOffset : adidBufPayloadOffset+adidBufPayloadLen]
-	for i := 0; i < adidCipherLen; i++ {
-		payload[i] = cipherText[i] ^ pad[i]
-	}
-
-	// Init encryption hmac.
-	if a.hmacI == nil {
-		a.hmacI = hmac.New(sha1.New, a.integrityKey)
-	}
-	// Compute signature.
-	computedSign := a.buf[adidBufSignOffset : adidBufSignOffset+adidBufSignLen]
-	a.hmacI.Reset()
-	a.hmacI.Write(payload)
-	a.hmacI.Write(initVector)
-	computedSign = a.hmacI.Sum(computedSign[:0])[:adidIntegritySignLen]
-	if !hmac.Equal(computedSign, integritySign) {
-		return dst, ErrSignCheckFail
+	var err error
+	dst, err = a.decrypt(dst, encryptedAdID, adidCipherLen)
+	if err != nil {
+		return dst, err
 	}
 
 	if uuid {
 		// Convert payload to UUID.
-		for i := 0; i < adidBufPayloadLen; i++ {
+		a.buf = append(a.buf[:0], dst...)
+		dst = dst[:0]
+		for i := 0; i < adidCipherLen; i++ {
 			switch i {
 			case uuidDashPosTimeLow, uuidDashPosTimeMid, uuidDashPosTimeHiAndVer, uuidDashPosClockSeq:
 				dst = append(dst, '-')
 			}
-			dst = append(dst, hextable[payload[i]>>4])
-			dst = append(dst, hextable[payload[i]&0x0f])
+			dst = append(dst, hextable[a.buf[i]>>4])
+			dst = append(dst, hextable[a.buf[i]&0x0f])
 		}
-	} else {
-		// Return raw payload.
-		dst = append(dst[:0], payload...)
 	}
 	return dst, nil
 }
--- doubleclick/doubleclick.go
+++ doubleclick/doubleclick.go
@@ -1,11 +1,23 @@
 package doubleclick
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
 	"errors"
 	"hash"
 )
 
 const (
+	initVectorOffset = 0
+	initVectorLen    = 16
+	cipherOffset     = 16
+	integritySignLen = 4
+
+	bufPadOffset     = 0
+	bufPadLen        = 20
+	bufPayloadOffset = 20
+	bufSignLen       = 20 // integrity signature needs only 4 bytes buffer, but hmac makes 20-bytes array by default
+
 	hextable = "0123456789abcdef"
 
 	uuidDashPosTimeLow      = 4
@@ -26,4 +38,51 @@ var (
 
 func (d *DoubleClick) SetKeys(encryptionKey, integrityKey []byte) {
 	d.encryptionKey, d.integrityKey = encryptionKey, integrityKey
+}
+
+func (d *DoubleClick) decrypt(dst, cipher []byte, payloadLen int) ([]byte, error) {
+	initVector := cipher[initVectorOffset:initVectorLen]
+	cipherText := cipher[cipherOffset : cipherOffset+payloadLen]
+	integritySignOffset := cipherOffset + payloadLen
+	integritySign := cipher[integritySignOffset : integritySignOffset+integritySignLen]
+
+	// Prepare buffer.
+	bufLen := bufPadLen + payloadLen + bufSignLen
+	if len(d.buf) < bufLen {
+		d.buf = append(d.buf, make([]byte, bufLen-len(d.buf))...)
+	}
+
+	// Init encryption hmac.
+	if d.hmacE == nil {
+		d.hmacE = hmac.New(sha1.New, d.encryptionKey)
+	}
+	// Compute pad.
+	pad := d.buf[bufPadOffset:bufPadLen]
+	d.hmacE.Reset()
+	d.hmacE.Write(initVector)
+	pad = d.hmacE.Sum(pad[:0])
+
+	// Apply xor to reverse encryption.
+	payload := d.buf[bufPayloadOffset : bufPayloadOffset+payloadLen]
+	for i := 0; i < payloadLen; i++ {
+		payload[i] = cipherText[i] ^ pad[i]
+	}
+
+	// Init encryption hmac.
+	if d.hmacI == nil {
+		d.hmacI = hmac.New(sha1.New, d.integrityKey)
+	}
+	// Compute signature.
+	bufSignOffset := bufPayloadOffset + payloadLen
+	computedSign := d.buf[bufSignOffset : bufSignOffset+bufSignLen]
+	d.hmacI.Reset()
+	d.hmacI.Write(payload)
+	d.hmacI.Write(initVector)
+	computedSign = d.hmacI.Sum(computedSign[:0])[:integritySignLen]
+	if !hmac.Equal(computedSign, integritySign) {
+		return dst, ErrSignCheckFail
+	}
+
+	dst = append(dst[:0], payload...)
+	return dst, nil
 }
